431c364439c2e58be4cc643ddbfdb5d8c3c2ffc4
mohammad.salama <mohammad.salama@hiast.edu.sy>
2024-08-05 14:43:31 +0300

    Added Dockerfiles and images were built correctly , depencies updated to match and work properly with asp,sdk,runtime version

--- MessageGeneratorGRPC/Program.cs
+++ MessageGeneratorGRPC/Program.cs
@@ -1,12 +1,18 @@
 using GGRPCMessageGenerator;
 using Steeltoe.Discovery.Client;
 
-IHost host = Host.CreateDefaultBuilder(args)
-            .ConfigureServices(services =>
-            {
-                services.AddHostedService<Worker>();
-                services.AddDiscoveryClient();
-            })
-            .Build();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                    services.AddDiscoveryClient();
+                })
+                .Build();
 
-host.Run();
+        host.Run();
+    }
+}
